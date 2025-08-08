08db6e76d2e8124fad7459d190628f5b834d14d9
Daniel Vilela Gonçalves <danielvg1136@gmail.com>
2025-08-08 19:21:33 -0300

    feat: create the type decorators module

---
+++ src/TA/support/infra/orm/type_decorators.py
@@ -0,0 +1,20 @@
+import uuid
+from sqlalchemy import VARCHAR
+from sqlalchemy.types import TypeDecorator
+
+class UUIDType(TypeDecorator):
+    impl = VARCHAR(36)
+
+    def process_bind_param(self, value, dialect):
+        if value is None:
+            return value
+        if isinstance(value, uuid.UUID):
+            return str(value)
+        return str(uuid.UUID(value))
+
+    def process_result_value(self, value, dialect):
+        if value is None:
+            return value
+        if not isinstance(value, uuid.UUID):
+            return uuid.UUID(value)
+        return value

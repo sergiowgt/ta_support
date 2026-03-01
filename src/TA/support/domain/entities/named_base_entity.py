6285bea5a1d3c5f5356727a892cbb3b9c307f676
sergiowgt <sergiowgt@gmail.com>
2026-03-01 19:56:40 -0300

    Revert "VERSAO COM  QUEBRA DO DDD"
    This reverts commit 9b326c8eeeb3114e527eac54dbc4691d2130621b.

--- src/TA/support/domain/entities/named_base_entity.py
+++ src/TA/support/domain/entities/named_base_entity.py
@@ -1,26 +1,12 @@
-# TA/support/domain/entities/named_base_entity.py
-
-from sqlalchemy import VARCHAR
-from sqlalchemy.orm import mapped_column, Mapped
-
+from dataclasses import dataclass, field
 from TA.support.domain.entities.base_entity import BaseEntity
 from TA.support.domain.enums.unique_type_enum import UniqueTypeEnum
 from TA.support.infra.validators.field_presets import NAME_FIELD
 
-
-class NamedBaseEntity(BaseEntity, init=False):
-    """
-    Base para entidades que possuem o campo 'name'.
-    Equivale ao NamedBaseEntity original, agora com mapped_column.
-    """
-    __abstract__ = True
-
-    name: Mapped[str] = mapped_column(
-        VARCHAR(NAME_FIELD.max_len), nullable=False,  # NAME_FIELD.max_len = 100
-        metadata={
-            'field_config': NAME_FIELD,
-            'required': True,
-            'display': 'Name',
-            'unique_type': UniqueTypeEnum.FIELD_ONLY,
-        }
-    )
+@dataclass
+class NamedBaseEntity(BaseEntity):
+    name: str = field(default='', 
+        metadata={'field_config': NAME_FIELD, 
+                  'required': True, 
+                  'display': 'Name', 
+                  'unique_type': UniqueTypeEnum.FIELD_ONLY})
